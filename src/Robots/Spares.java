f3fa4fd820a26cfe63d64be24d31d611e5cce6f6
Sergey060890 <sergey-real90@mail.ru>
2022-03-14 23:55:34 +0300

    Lesson_Robot

---
+++ src/Robots/Spares.java
@@ -0,0 +1,16 @@
+package Robots;
+
+import java.util.Random;
+
+
+//создали enum с запчастями
+public enum Spares {
+    HEAD, BODY, LEFT_ARM, RIGHT_ARM, LEFT_LEG, RIGHT_LEG, CPU, RAM, HDD;
+
+    //выбираем рандомно запчасть
+    public static Spares getRandomSpares() {
+        Random random = new Random();
+        Spares[] takeSpare = Spares.values();
+        return takeSpare[random.nextInt(takeSpare.length)];
+    }
+}
